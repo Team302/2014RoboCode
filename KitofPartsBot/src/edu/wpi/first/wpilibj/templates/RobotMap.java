a2a65c48779a20d9386176ddf4a6bf32be215895
Draghignazzo <johnfb11@gmail.com>
2014-01-30 19:20:44 -0500

    Updated variables in the RobotMap class.

--- KitofPartsBot/src/edu/wpi/first/wpilibj/templates/RobotMap.java
+++ KitofPartsBot/src/edu/wpi/first/wpilibj/templates/RobotMap.java
@@ -18,10 +18,10 @@ public interface RobotMap {
     static final int PWM_RIGHT_MOTOR_2 = 4;
     
     static final int DIO_PRESSURE_SWITCH = 1;
-    static final int DIO_LEFT_ENCODER_ACHANNEL = 10;
-    static final int DIO_LEFT_ENCODER_BCHANNEL = 11;
     static final int DIO_RIGHT_ENCODER_ACHANNEL = 3;
     static final int DIO_RIGHT_ENCODER_BCHANNEL = 4;
+    static final int DIO_LEFT_ENCODER_ACHANNEL = 10;
+    static final int DIO_LEFT_ENCODER_BCHANNEL = 11;
     
     static final int RELAY_COMPRESSOR = 2;
     
